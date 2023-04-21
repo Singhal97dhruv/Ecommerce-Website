93c3e15bd265de660fb85a4001a7da6ec47f4c52
Dhruv <Dhruvsinghal9876@gmail.com>
2023-04-21 14:27:02 +0530

    for vercel

--- backend/app.js
+++ backend/app.js
@@ -30,9 +30,13 @@ app.use("/api/v1",payment);
 
 app.use(express.static(path.join(__dirname,"../frontend/build")))
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
-})
+if(process.env.NODE_ENV=='production'){
+    const path=require('path')
+    app.get("*",(req,res)=>{
+        app.use(express.static(path.resolve(__dirname,'client','build')))
+        res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
+    })
+}
 
 //Middleware for errors
 app.use(errorMiddleware);
